683e7d78b02a04fcb41573028d75dd49a360b0d1
AmierHajyassin <amierhajyassin89@yahoo.com>
2023-04-04 22:16:51 -0400

    Updating Week 13

--- src/main/java/com/promineotech/jeep/entity/JeepModel.java
+++ src/main/java/com/promineotech/jeep/entity/JeepModel.java
@@ -1,10 +1,8 @@
 package com.promineotech.jeep.entity;
 
 public enum JeepModel {
-  Grand_CHEROKEE,
+  GRAND_CHEROKEE,
   CHEROKEE,
-  RENEGADE,
-  COMPASS,
   WRANGLER,
   WRANGLER_4XE,
   GLADIATOR
